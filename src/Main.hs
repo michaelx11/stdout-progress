467ac5ab710feba697bbfb14591c27c3fc3fb7f8
Michael Xu <michaeljxu11@gmail.com>
2017-10-06 23:57:07 -0700

    dealing with OverloadedStrings issue

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
---import           Control.Applicative      ((*>))
---import           Control.Concurrent.Async (Concurrently (..))
---import           Data.Conduit             (await, yield, ($$), (=$))
---import qualified Data.Conduit.Binary      as CB
---import qualified Data.Conduit.List        as CL
---import           Data.Conduit.Process     (ClosedStream (..), streamingProcess,
---                                           proc, waitForStreamingProcess)
+import           Control.Applicative      ((*>))
+import           Control.Concurrent.Async (Concurrently (..))
+import           Data.Conduit
+--import           Data.Conduit             (await, yield, ($$), (=$), Sink, Conduit, ($=), (=$=), getZipSink, ZipSink)
+import qualified Data.Conduit.Binary      as CB
+import           Data.Conduit.Process     (ClosedStream (..), streamingProcess,
+                                           proc, waitForStreamingProcess)
 import           System.IO                (stdin, stdout)
 import           System.Environment
 import           System.Exit
---
---import           ClassyPrelude.Conduit
-
-import Data.Conduit
 import qualified Data.Conduit.List as C
 import qualified Data.Foldable as F
 
@@ -53,84 +50,66 @@ lengthSum = do
 lengthSink :: Sink String IO ()
 lengthSink = C.mapM_ putStrLn
 
--- | An example that produces indexed output.
-testSource :: (Monad m) => Source m (String)
-testSource = do
-    yield "test"
-    yield "what"
-    yield "sup"
+-- -- | An example that produces indexed output.
+-- testSource :: (Monad m) => Source m (String)
+-- testSource = do
+--     yield "test"
+--     yield "what"
+--     yield "sup"
 
-main :: IO ()
-main = do
-    let countLoop = do
+main = getArgs >>= parse 
+
+parse ["-h"] = usage   >> exit
+parse ["-v"] = version >> exit
+parse []     = usage >> exit
+parse ("-b":nb:cmds)     = putStrLn $ show nb
+                        
+parse cmds   = do
+    ((toProcess, close), fromProcess, fromStderr, cph) <-
+        streamingProcess (proc "ls" [])
+--        streamingProcess (proc (head cmds) (tail cmds))
+
+    let input = CB.sourceHandle stdin
+             $$ CB.lines
+             =$ inputLoop
+             =$ toProcess
+
+        inputLoop = do
+            close
+--            mbs <- await
+--            case mbs of
+--                Nothing -> close
+--                Just "quit" -> close
+--                Just bs -> do
+--                    yield bs
+--                    inputLoop
+
+--        output = fromProcess $$ CL.mapM_
+--            (\bs -> putStrLn $ "from process: " ++ show bs)
+        output = fromProcess
+            $$ splitLoop =$ multiSink_ [(testSink "orig:"), (lengthMap $= lengthSum $= lengthSink)]
+
+        splitLoop = do
             mbs <- await
             case mbs of
                 Nothing -> return ()
                 Just bs -> do
                     yield (0, bs)
                     yield (1, bs)
-                    countLoop
+                    splitLoop
 
-    testSource
-      $$ countLoop
-      =$ multiSink_ [(testSink "orig:"), (lengthMap $= lengthSum $= lengthSink)]
--- main = getArgs >>= parse 
--- 
--- parse ["-h"] = usage   >> exit
--- parse ["-v"] = version >> exit
--- parse []     = usage >> exit
--- parse ("-b":nb:cmds)     = putStrLn $ show nb
---                         
--- parse cmds   = do
---     ((toProcess, close), fromProcess, fromStderr, cph) <-
---         streamingProcess (proc "ls" [])
--- --        streamingProcess (proc (head cmds) (tail cmds))
--- 
---     let input = CB.sourceHandle stdin
---              $$ CB.lines
---              =$ inputLoop
---              =$ toProcess
--- 
---         inputLoop = do
---             close
--- --            mbs <- await
--- --            case mbs of
--- --                Nothing -> close
--- --                Just "quit" -> close
--- --                Just bs -> do
--- --                    yield bs
--- --                    inputLoop
--- 
--- --        output = fromProcess $$ CL.mapM_
--- --            (\bs -> putStrLn $ "from process: " ++ show bs)
---         output = fromProcess
---             $$ CB.lines
---             =$ countLoop
---             =$ CB.sinkHandle stdout
--- 
---         countLoop = do
---             mbs <- await
---             case mbs of
---                 Nothing -> do
---                     yield "yo"
---                     yield "\n"
---                 Just bs -> do
---                     yield bs
---                     yield "\n"
---                     countLoop
--- 
---         errout = fromStderr $$ CL.mapM_
---             (\bs -> putStrLn $ "from stderr: " ++ show bs)
--- 
---     ec <- runConcurrently $
---         Concurrently input *>
---         Concurrently output *>
---         Concurrently errout *>
---         Concurrently (waitForStreamingProcess cph)
--- 
---     putStrLn $ "Process exit code: " ++ show ec
--- 
--- usage   = putStrLn "Usage: stdout-progress [-vh] [-b num bytes] [cmd ... args]"
--- version = putStrLn "stdout-progress 0.1"
--- exit    = exitWith ExitSuccess
--- die     = exitWith (ExitFailure 1)
+        errout = fromStderr $$ C.mapM_
+            (\bs -> putStrLn $ "from stderr: " ++ show bs)
+
+    ec <- runConcurrently $
+        Concurrently input *>
+        Concurrently output *>
+        Concurrently errout *>
+        Concurrently (waitForStreamingProcess cph)
+
+    putStrLn $ "Process exit code: " ++ show ec
+
+usage   = putStrLn "Usage: stdout-progress [-vh] [-b num bytes] [cmd ... args]"
+version = putStrLn "stdout-progress 0.1"
+exit    = exitWith ExitSuccess
+die     = exitWith (ExitFailure 1)
